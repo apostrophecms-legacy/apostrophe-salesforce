fcf048ffd22f8d7bd35990cc38dc1bbb01b1472b
Jimmy Harrison <jimmy@punkave.com>
2014-12-23 09:30:10 -0500

    new branch for major refactor of callback hell. not functional at the moment

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var _ = require('lodash'),
+var _       = require('lodash'),
+    async   = require('async'),
     flatten = require('flat'),
     jsforce = require('jsforce');
 
 module.exports = salesforce;
 
-function salesforce(options, callback) {
+function salesforce (options, callback) {
   return new Construct(options, callback);
 }
 
-function Construct(options, callback) {
+function Construct (options, callback) {
   var self = this;
-  // Add a bunch of methods to self here, then...
+
+  // Add a bunch of methods to self here
   self._app = options.app;
   self._apos = options.apos;
   self._site = options.site;
@@ -18,107 +20,33 @@ function Construct(options, callback) {
   // The configuration of how Salesforce data maps into Apostrophe
   self.mappings = options.mappings;
 
-  self._apos.mixinModuleAssets(self, 'salesforce', __dirname, options);
-
   // Salesforce authentication stuffs
   self.sfUsername = options.sfUsername;
   self.sfPassword = options.sfPassword;
 
-  self._app.get("/salesforce/sync", function(req, res) {
-
-    var conn = new jsforce.Connection({});
-
-    conn.login(self.sfUsername, self.sfPassword, function(err, userInfo) {
-      if(err) console.log(err);
-      self.mappings.forEach(function(mapping) {
-        // Get the relevant Apostrophe object for the mapping
-        var Type = self._site.modules[mapping.aposObj];
-
-        // Build SOQL Query
-        var queryFields = [];
-        var whereClauses = [];
-        for(aposField in mapping.fields) {
-          sfFields = mapping.fields[aposField];
-          if(!(sfFields instanceof Array)) {
-            sfFields = [sfFields];
-          }
-          queryFields.push.apply(queryFields, sfFields);
-
-          // Add required field stipulations
-          if(mapping.required && mapping.required.indexOf(aposField) >= 0) {
-            sfFields.forEach(function(sfField) {
-              whereClauses.push(sfField + " != null")
-            });
-          }
-        }
-        if(mapping.where) {
-          mapping.where.forEach(function(whereClause) {
-            whereClauses.push(whereClause);
-          });
-        }
-        // Should add some configurable criteria, e.g. custom flag that allows records to be imported by Apostrophe
-        var queryString = "SELECT Id, " + queryFields.join(', ') + 
-                          " FROM " + mapping.sfObj + 
-                          ((whereClauses.length > 0) ? " WHERE " + whereClauses.join(" AND ") : "")
-                          + " LIMIT 1000";
-        //console.log(queryString);
-
-        // Execute query
-        conn.query(queryString, function(err, result) {
-          if (err) console.log(err);
-          result.records.forEach(function(sfObj) {
-            // To deal with addressing nested elements
-            sfObj = flatten(sfObj, {safe: true});
-
-            // Create new instance of object and associate with Salesforce id
-            var aposObj = Type.newInstance();
-            aposObj.sfId = sfObj.Id;
-
-            // Add fields to Apostrophe object according to mapping configuration
-            for (aposField in mapping.fields) {
-              var sfFields = mapping.fields[aposField];
-              if(!(sfFields instanceof Array)) {
-                sfFields = [sfFields];
-              }
-              var sfFieldValues = [];
-              sfFields.forEach(function(sfField) {
-                var sfValue = sfObj[sfField];
-                if(typeof sfValue !== 'undefined' && sfValue !== null) {
-                  sfFieldValues.push(sfValue);
-                }
-              });
-              // Concatenate selected fields and add them to Apostrophe object
-              if(sfFieldValues.length > 0) {
-                aposObj[aposField] = sfFieldValues.join(' ');
-              }
-            }
-
-            // De-duping step
-            Type.getOne(req, {sfId: aposObj.sfId}, {}, function(err, item) {
-              if(err) {
-                console.error(err);
-              }
-              if(!item) {
-                item = aposObj;
-              } else {
-                _.extend(item, aposObj);
-              }
-              // Save the Apostrophe object
-              Type.putOne(req, {}, item, function(err) {
-                if(err) {
-                  console.error(err);
-                }
-                // saved from salesforce successfully!
-              });
-            });
-          });
-        });
+  self._app.get("/apos/salesforce/sync", function(req, res) {
+    withConnection(function (err, connection) {
+      if (err) callback(err);
+      var queries = {};
+      for (i in mappings) {
+        var mapping = mappings[i];
+        mapping.Type = self._site.modules[mapping.aposObj];
+        queries[mapping] = new Query(mapping);
       });
+      async.parallel()
     });
-
     res.redirect('/');
   });
 
+  function withConnection (callback) {
+    var connection = new jsforce.Connection({});
+    connection.login(self.sfUsername, self.sfPassword, function(err, userInfo) {
+      return callback(err, connection);
+    });
+  }
+
+  // Add admin UI elements
+  self._apos.mixinModuleAssets(self, 'salesforce', __dirname, options);
   self._apos.addLocal('aposSalesforceMenu', function(args) {
     var result = self.render('menu', args);
     return result;
@@ -129,6 +57,113 @@ function Construct(options, callback) {
     return process.nextTick(function() {
       return callback(null);
     });
+  }
+}
+
+function Query = function (mapping) {
+  var self = this;
+
+  var Type = mapping.Type;
+
+  // Construct the SOQL Query
+  var queryFields = [];
+  var whereClauses = [];
+  // Add fields to SELECT
+  for(aposField in mapping.fields) {
+    var sfFields = mapping.fields[aposField];
+    if(!(sfFields instanceof Array)) {
+      sfFields = [sfFields];
+    }
+    queryFields.push.apply(queryFields, sfFields);
+    // Add required fields
+    if(mapping.required && mapping.required.indexOf(aposField) >= 0) {
+      sfFields.forEach(function(sfField) {
+        whereClauses.push(sfField + " != null")
+      });
+    }
+  }
+  // Add nested queries for JOINs
+  for(aposJoin in mapping.joins) {
+    var sfJoin = mapping.joins[aposJoin];
+    queryFields.push('(SELECT Entity.Id FROM ' + sfJoin + ' AS Entity)');
+  }
+  // Add custom WHERE clauses
+  if (mapping.where) {
+    whereFields.push.apply(whereFields, mapping.where)
+  }
+  // Create query string
+  self.queryString = "SELECT Id, " + queryFields.join(', ') + 
+                    " FROM " + mapping.sfObj + 
+                    ((whereClauses.length > 0) ? " WHERE " + whereClauses.join(" AND ") : "") +
+                    " LIMIT 1000";
+
+  self.execute = function(callback) {
+    // Execute the query
+    conn.query(self.queryString, function(err, result) {
+      if(err) callback(err);
+      result.records.forEach(function(sfObj) {
+        // To deal with addressing nested elements
+        sfObj = flatten(sfObj, {safe: true});
+
+        // Create new instance of object and associate with Salesforce id
+        var aposObj = Type.newInstance();
+        aposObj.sfId = sfObj.Id;
+
+        // Add fields to Apostrophe object according to mapping configuration
+        for(aposField in mapping.fields) {
+          var sfFields = mapping.fields[aposField];
+          if(!(sfFields instanceof Array)) {
+            sfFields = [sfFields];
+          }
+          var sfFieldValues = [];
+          sfFields.forEach(function(sfField) {
+            var sfValue = sfObj[sfField];
+            if(typeof sfValue !== 'undefined' && sfValue !== null) {
+              sfFieldValues.push(sfValue);
+            }
+          });
+          // Concatenate selected fields and add them to Apostrophe object
+          if(sfFieldValues.length > 0) {
+            aposObj[aposField] = sfFieldValues.join(' ');
+          }
+        }
+        for(aposJoin in mapping.joins) {
+          var sfJoin = mapping.joins[aposJoin];
+          var joinResults = sfObj[sfJoin + '.records'];
+          for(i in joinResults) {
+            console.log(joinResults[i].Id);
+
+          }
+        }
+
+        // De-duping step
+        Type.getOne(req, {sfId: aposObj.sfId}, {}, function(err, item) {
+          if(err) {
+            console.error(err);
+          }
+          if(!item) {
+            item = aposObj;
+          } else {
+            _.extend(item, aposObj);
+          }
+          // Save the Apostrophe object
+          Type.putOne(req, {}, item, function(err) {
+            if(err) {
+              console.error(err);
+            }
+            // saved from salesforce successfully!
+          });
+        });
+      });
+    });
+  }
+
+  self.save = function(callback) {
+
+  }
+
+  self.join = function(callback) {
+
   }
 }
 
